06545d35e7da7f853a876eaf2a873799a504d452
hongthu <hongthu24032001@gmail.com>
2023-11-14 14:04:18 +0700

    fix UI and edit code

--- js_reminder/main.js
+++ js_reminder/main.js
@@ -1,5 +1,5 @@
 import { renderListOnUI } from "./js/UI_controller/list_controller.js";
-import { initState } from "./js/global/init.js";
+import { initState } from "./js/global/initState.js";
 import { formAddList } from "./js/UI_controller/list_form.js";
 import { addNewReminderForm } from "./js/UI_controller/reminder_form.js";
 
